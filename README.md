ae3f343422f5af6e91cdf12a484171b9172519e8
Isabel de Luis <ideluis@mit.edu>
2026-04-05 16:46:15 -0400

    added initial files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 6.C35_Final_Project
 Final Project For  6.C35/85
+
+Install depemndencies:
+
+Open VS Code terminal and run:
+`npm install`
+
+Once npm install finishes, run:
+```
+npm run dev -- --open
+```
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Contemporary Effects of Redlining on MBTA Development</h1>
+<script>
+</script>
+
+<section class="hero">
+  <div class="hero-content">
+    <h1 class="title">
+      CONTEMPORARY<br />
+      EFFECTS OF<br />
+      REDLINING ON MBTA<br />
+      DEVELOPMENT
+    </h1>
+
+    <p class="authors">Isa de Luis, Unyi Usua, Rodrigo Gallardo</p>
+
+    <p class="scroll-label">SCROLL TO FOLLOW THE TIMELINE ↓</p>
+
+    <div class="arrow">↓</div>
+
+    <p class="description">
+      Visualizations are organized by time period, data flows chronologically so
+      viewers can naturally see change over a particular time period.
+    </p>
+  </div>
+</section>
